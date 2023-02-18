c40f22bc8cad2eb04735a53b3987da38bd19c64c
AhamedAfq <ashfakahamed142@gmail.com>
2023-02-18 23:15:11 +0530

    Displaying recipe details

--- src/app/recepies/recepies-list/recepies-list.component.ts
+++ src/app/recepies/recepies-list/recepies-list.component.ts
@@ -8,6 +8,7 @@ import { Recepies } from '../recepies.model';
 })
 export class RecepiesListComponent implements OnInit{
   recepies: Recepies[] = [
+    new Recepies('A Test recepie','desc test','https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg'),
     new Recepies('A Test recepie','desc test','https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
   ];
   constructor(){
