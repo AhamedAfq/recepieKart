e617ace13f26d0cab8ab2954c4e1e330e8323133
AhamedAfq <ashfakahamed142@gmail.com>
2023-02-18 22:20:26 +0530

    outputting the list of recepies with ngFor

--- src/app/recepies/recepies-list/recepies-list.component.ts
+++ src/app/recepies/recepies-list/recepies-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recepies } from '../recepies.model';
 
 @Component({
   selector: 'app-recepies-list',
   templateUrl: './recepies-list.component.html',
   styleUrls: ['./recepies-list.component.css']
 })
-export class RecepiesListComponent {
+export class RecepiesListComponent implements OnInit{
+  recepies: Recepies[] = [
+    new Recepies('A Test recepie','desc test','https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
+  ];
+  constructor(){
 
+  }
+  ngOnInit(){
+      
+  }
 }
+
