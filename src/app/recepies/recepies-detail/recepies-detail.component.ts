e9c4c2de8e19671a73c10ece03e5841e2835aae5
AhamedAfq <ashfakahamed142@gmail.com>
2023-02-18 18:54:01 +0530

    Initial commit - Also has header changes

---
+++ src/app/recepies/recepies-detail/recepies-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepies-detail',
+  templateUrl: './recepies-detail.component.html',
+  styleUrls: ['./recepies-detail.component.css']
+})
+export class RecepiesDetailComponent {
+
+}
---
+++ src/app/recepies/recepies-list/recepies-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepies-list',
+  templateUrl: './recepies-list.component.html',
+  styleUrls: ['./recepies-list.component.css']
+})
+export class RecepiesListComponent {
+
+}
